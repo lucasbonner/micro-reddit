169e94932379cb3cd09bd0856d50079087e61ce4
lucasbonner <lucasbonner@protonmail.com>
2023-12-27 13:30:29 -0600

    Finished micro-reddit exercise
    Currently no front-end, just practice with models & associations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,7 @@
 class User < ApplicationRecord
+  has_many :posts
+  has_many :comments
+
+  validates :username, :password, presence: true
+  validates :username, length: { minimum: 5 }, uniqueness: true
 end
